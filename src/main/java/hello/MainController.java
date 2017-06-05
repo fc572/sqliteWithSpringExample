68323716a3ee499eaccd3ef38e4a3df72d26f8e7
francesco <francesco.calvino@gmail.com>
2017-06-05 14:53:01 +0100

    first commit

---
+++ src/main/java/hello/MainController.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demo")
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String password){
+        User n = new User();
+        n.setName(name);
+        n.setPassword(password);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
